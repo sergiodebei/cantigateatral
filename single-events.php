bd8e224c4c56384612ce7a45ba08098bb1d2c116
sergio_de_bei <sergiodebei84@gmai.com>
2018-09-20 03:32:53 +0200

    Add single event template

---
+++ single-events.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The Template for displaying all single events
+ *
+ */
+
+$template               = 'single.twig';
+$post                   = Timber::get_post();
+$context                = Timber::get_context();
+$context['post']        = $post;
+$context['post']->blocks = $post->get_field('blocks');
+$context['body_class']  = 'page-single';
+
+Timber::render('single-events.twig', $context);
