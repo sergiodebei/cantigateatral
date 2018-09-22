006fc1be66f9f184ce44555572095509f3eebbfd
sergio_de_bei <sergiodebei84@gmai.com>
2018-09-22 16:44:51 +0200

    Add remove menu post and comment

--- functions/theme.php
+++ functions/theme.php
@@ -39,6 +39,7 @@ class StarterSite extends TimberSite {
     // add_action( 'init', array($this, 'enqueue_styles'));
     // add_action( 'init', array($this, 'enqueue_scripts'));
     add_filter( 'timber_context', array( $this, 'add_to_context' ) );
+    add_action( 'admin_init', array($this, 'post_remove'));
     add_action( 'init', array( $this, 'register_post_types' ) );
     // add_action( 'init', array( $this, 'register_taxonomies' ) );
     // add_action( 'init', array($this, 'option_page') );
@@ -104,6 +105,20 @@ class StarterSite extends TimberSite {
         $context['site']->is_home = is_front_page();
         // $context['options'] = get_fields('options');
         return $context;
+    }
+
+    function post_remove(){
+        // remove_menu_page( 'index.php' );                  //Dashboard
+        // remove_menu_page( 'jetpack' );                    //Jetpack* 
+        remove_menu_page( 'edit.php' );                   //Posts
+        // remove_menu_page( 'upload.php' );                 //Media
+        // remove_menu_page( 'edit.php?post_type=page' );    //Pages
+        remove_menu_page( 'edit-comments.php' );          //Comments
+        // remove_menu_page( 'themes.php' );                 //Appearance
+        // remove_menu_page( 'plugins.php' );                //Plugins
+        // remove_menu_page( 'users.php' );                  //Users
+        // remove_menu_page( 'tools.php' );                  //Tools
+        // remove_menu_page( 'options-general.php' );        //Settings
     }
 
     // Register Custom Post Type
@@ -285,74 +300,74 @@ function nlDate($datum){
     $parameters = str_replace("a", "", $parameters);
     $datum = date($parameters);
     */
-    // Vervang de maand, klein
-    $datum = str_replace("january",     "januari",         $datum);
-    $datum = str_replace("february",     "februari",     $datum);
-    $datum = str_replace("march",         "maart",         $datum);
-    $datum = str_replace("april",         "april",         $datum);
-    $datum = str_replace("may",         "mei",             $datum);
-    $datum = str_replace("june",         "juni",         $datum);
-    $datum = str_replace("july",         "juli",         $datum);
-    $datum = str_replace("august",         "augustus",     $datum);
-    $datum = str_replace("september",     "september",     $datum);
-    $datum = str_replace("october",     "oktober",         $datum);
-    $datum = str_replace("november",     "november",     $datum);
-    $datum = str_replace("december",     "december",     $datum);
+    // // Vervang de maand, klein
+    // $datum = str_replace("january",     "januari",         $datum);
+    // $datum = str_replace("february",     "februari",     $datum);
+    // $datum = str_replace("march",         "maart",         $datum);
+    // $datum = str_replace("april",         "april",         $datum);
+    // $datum = str_replace("may",         "mei",             $datum);
+    // $datum = str_replace("june",         "juni",         $datum);
+    // $datum = str_replace("july",         "juli",         $datum);
+    // $datum = str_replace("august",         "augustus",     $datum);
+    // $datum = str_replace("september",     "september",     $datum);
+    // $datum = str_replace("october",     "oktober",         $datum);
+    // $datum = str_replace("november",     "november",     $datum);
+    // $datum = str_replace("december",     "december",     $datum);
 
-    // Vervang de maand, hoofdletters
-    $datum = str_replace("January",     "Januari",         $datum);
-    $datum = str_replace("February",     "Februari",     $datum);
-    $datum = str_replace("March",         "Maart",         $datum);
-    $datum = str_replace("April",         "April",         $datum);
-    $datum = str_replace("May",         "Mei",             $datum);
-    $datum = str_replace("June",         "Juni",         $datum);
-    $datum = str_replace("July",         "Juli",         $datum);
-    $datum = str_replace("August",         "Augustus",     $datum);
-    $datum = str_replace("September",     "September",     $datum);
-    $datum = str_replace("October",     "Oktober",         $datum);
-    $datum = str_replace("November",     "November",     $datum);
-    $datum = str_replace("December",     "December",     $datum);
+    // // Vervang de maand, hoofdletters
+    // $datum = str_replace("January",     "Januari",         $datum);
+    // $datum = str_replace("February",     "Februari",     $datum);
+    // $datum = str_replace("March",         "Maart",         $datum);
+    // $datum = str_replace("April",         "April",         $datum);
+    // $datum = str_replace("May",         "Mei",             $datum);
+    // $datum = str_replace("June",         "Juni",         $datum);
+    // $datum = str_replace("July",         "Juli",         $datum);
+    // $datum = str_replace("August",         "Augustus",     $datum);
+    // $datum = str_replace("September",     "September",     $datum);
+    // $datum = str_replace("October",     "Oktober",         $datum);
+    // $datum = str_replace("November",     "November",     $datum);
+    // $datum = str_replace("December",     "December",     $datum);
 
-    // Vervang de maand, kort
-    $datum = str_replace("Jan",         "Jan",             $datum);
-    $datum = str_replace("Feb",         "Feb",             $datum);
-    $datum = str_replace("Mar",         "Maa",             $datum);
-    $datum = str_replace("Apr",         "Apr",             $datum);
-    $datum = str_replace("May",         "Mei",             $datum);
-    $datum = str_replace("Jun",         "Jun",             $datum);
-    $datum = str_replace("Jul",         "Jul",             $datum);
-    $datum = str_replace("Aug",         "Aug",             $datum);
-    $datum = str_replace("Sep",         "Sep",             $datum);
-    $datum = str_replace("Oct",         "Ok",             $datum);
-    $datum = str_replace("Nov",         "Nov",             $datum);
-    $datum = str_replace("Dec",         "Dec",             $datum);
+    // // Vervang de maand, kort
+    // $datum = str_replace("Jan",         "Jan",             $datum);
+    // $datum = str_replace("Feb",         "Feb",             $datum);
+    // $datum = str_replace("Mar",         "Maa",             $datum);
+    // $datum = str_replace("Apr",         "Apr",             $datum);
+    // $datum = str_replace("May",         "Mei",             $datum);
+    // $datum = str_replace("Jun",         "Jun",             $datum);
+    // $datum = str_replace("Jul",         "Jul",             $datum);
+    // $datum = str_replace("Aug",         "Aug",             $datum);
+    // $datum = str_replace("Sep",         "Sep",             $datum);
+    // $datum = str_replace("Oct",         "Ok",             $datum);
+    // $datum = str_replace("Nov",         "Nov",             $datum);
+    // $datum = str_replace("Dec",         "Dec",             $datum);
 
-    // Vervang de dag, klein
-    $datum = str_replace("monday",         "maandag",         $datum);
-    $datum = str_replace("tuesday",     "dinsdag",         $datum);
-    $datum = str_replace("wednesday",     "woensdag",     $datum);
-    $datum = str_replace("thursday",     "donderdag",     $datum);
-    $datum = str_replace("friday",         "vrijdag",         $datum);
-    $datum = str_replace("saturday",     "zaterdag",     $datum);
-    $datum = str_replace("sunday",         "zondag",         $datum);
+    // // Vervang de dag, klein
+    // $datum = str_replace("monday",         "maandag",         $datum);
+    // $datum = str_replace("tuesday",     "dinsdag",         $datum);
+    // $datum = str_replace("wednesday",     "woensdag",     $datum);
+    // $datum = str_replace("thursday",     "donderdag",     $datum);
+    // $datum = str_replace("friday",         "vrijdag",         $datum);
+    // $datum = str_replace("saturday",     "zaterdag",     $datum);
+    // $datum = str_replace("sunday",         "zondag",         $datum);
 
-    // Vervang de dag, hoofdletters
-    $datum = str_replace("Monday",         "Maandag",         $datum);
-    $datum = str_replace("Tuesday",     "Dinsdag",         $datum);
-    $datum = str_replace("Wednesday",     "Woensdag",     $datum);
-    $datum = str_replace("Thursday",     "Donderdag",     $datum);
-    $datum = str_replace("Friday",         "Vrijdag",         $datum);
-    $datum = str_replace("Saturday",     "Zaterdag",     $datum);
-    $datum = str_replace("Sunday",         "Zondag",         $datum);
+    // // Vervang de dag, hoofdletters
+    // $datum = str_replace("Monday",         "Maandag",         $datum);
+    // $datum = str_replace("Tuesday",     "Dinsdag",         $datum);
+    // $datum = str_replace("Wednesday",     "Woensdag",     $datum);
+    // $datum = str_replace("Thursday",     "Donderdag",     $datum);
+    // $datum = str_replace("Friday",         "Vrijdag",         $datum);
+    // $datum = str_replace("Saturday",     "Zaterdag",     $datum);
+    // $datum = str_replace("Sunday",         "Zondag",         $datum);
 
-    // Vervang de verkorting van de dag, hoofdletters
-    $datum = str_replace("Mon",            "Maa",             $datum);
-    $datum = str_replace("Tue",         "Din",             $datum);
-    $datum = str_replace("Wed",         "Woe",             $datum);
-    $datum = str_replace("Thu",         "Don",             $datum);
-    $datum = str_replace("Fri",         "Vri",             $datum);
-    $datum = str_replace("Sat",         "Zat",             $datum);
-    $datum = str_replace("Sun",         "Zon",             $datum);
+    // // Vervang de verkorting van de dag, hoofdletters
+    // $datum = str_replace("Mon",            "Maa",             $datum);
+    // $datum = str_replace("Tue",         "Din",             $datum);
+    // $datum = str_replace("Wed",         "Woe",             $datum);
+    // $datum = str_replace("Thu",         "Don",             $datum);
+    // $datum = str_replace("Fri",         "Vri",             $datum);
+    // $datum = str_replace("Sat",         "Zat",             $datum);
+    // $datum = str_replace("Sun",         "Zon",             $datum);
 
     return $datum;
 }
