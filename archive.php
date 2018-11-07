52952c215e5501a142a05cfd066a81473d376c9b
Sergio De Bei <sergio@festina.xxx>
2018-11-07 16:48:20 +0100

    Add agenda archive page

---
+++ archive.php
@@ -0,0 +1,35 @@
+<?php
+/**
+* The template for displaying Archive pages.
+*
+*/
+
+if ( ! class_exists( 'Timber' ) ) {
+ echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
+ return;
+}
+
+
+$templates = array( 'archive.twig' );
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+// $pageID = get_the_ID();
+// 356
+// $context['blocks'] = get_field('blocks', 356);
+//TODO the id show be dynamic
+$context['post']->blocks = get_field('blocks', 356);
+
+// $context['body_class']  = 'archive-'.$post->post_name;
+
+$context['events']    = Timber::get_posts( 
+    [ 
+      'post_type'   => 'events', 
+      'orderby'     => 'menu_order',
+      'order'       => 'ASC',
+      'numberposts' => '-1'
+    ]
+  );
+
+Timber::render( $templates, $context );
+ 
